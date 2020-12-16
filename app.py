1dd279175fcbec1da7f4e1e03bca222bb1f16031
Sebastjan <sebastjancizel@gmail.com>
2020-12-16 20:28:41 +0100

    refactors and cleanup

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ import calendar
 import json
 import os
 import vaex
+
 # plotly components
 import plotly
 import plotly.graph_objs as go
 import plotly.express as px
 import plotly.offline as offline
 from plotly.graph_objs import Scatter, Figure, Layout
+
 # import dash components
 import dash
 from dash.dependencies import Input, Output, State
@@ -23,11 +25,13 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = [dbc.themes.COSMO]
 
-app = dash.Dash(__name__,
-                external_stylesheets=external_stylesheets,
-                meta_tags=[
-                    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
-                ])
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
+)
 
 app.title = "NYC Taxi Data Dash"
 
@@ -39,33 +43,33 @@ STYLE_FILE = "mapbox://styles/scizel/ckhhw5ql12qs219pbajeadpss"
 px.set_mapbox_access_token(TOKEN)
 
 
-geo_filename = 'aux_data/taxi_zones-tiny.json'
+geo_filename = "aux_data/taxi_zones-tiny.json"
 with open(geo_filename) as f:
     geo_json = json.load(f)
-zone_filename = 'aux_data/zone.json'
+zone_filename = "aux_data/zone.json"
 with open(zone_filename) as f:
     zmapper = json.load(f)
-with open('aux_data/borough.json', 'rb') as f:
+with open("aux_data/borough.json", "rb") as f:
     bmapper = json.load(f)
-with open('aux_data/zone_to_borough.json', 'rb') as f:
+with open("aux_data/zone_to_borough.json", "rb") as f:
     zbmapper = json.load(f)
 
-data_filename = 'aux_data/zone_data.json'
-with open(data_filename, 'r') as f:
+data_filename = "aux_data/zone_data.json"
+with open(data_filename, "r") as f:
     ZONE_DATA = {int(zone): data for zone, data in json.load(f).items()}
 
 
-DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 CHART_LAYOUT = dict(
     title=None,
     margin={"r": 0, "l": 0, "b": 0, "t": 0},
-    plot_bgcolor='rgba(0,0,0,0)',
-    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor="rgba(0,0,0,0)",
+    paper_bgcolor="rgba(0,0,0,0)",
 )
 
-with open("README.md", 'r') as f:
-    DESCRIPTION = f.read() 
+with open("README.md", "r") as f:
+    DESCRIPTION = f.read()
 
 #
 #   Define chart builder functions
@@ -76,30 +80,35 @@ with open("README.md", 'r') as f:
 
 def create_figure_geomap(pickup_counts, zoom=10, center={"lat": 40.7, "lon": -73.99}):
     geomap_data = {
-        'count': pickup_counts,
-        'log_count': np.log10(pickup_counts),
-        'zone_name': list(zmapper.values())
+        "count": pickup_counts,
+        "log_count": np.log10(pickup_counts),
+        "zone_name": list(zmapper.values()),
     }
-    fig = px.choropleth_mapbox(geomap_data,
-                               geojson=geo_json,
-                               color="log_count",
-                               color_continuous_scale="RdYlBu_r",
-                               locations="zone_name",
-                               featureidkey="properties.zone",
-                               mapbox_style=STYLE_FILE,
-                               hover_data=['count'],
-                               zoom=zoom,
-                               center=center,
-                               opacity=0.7,
-                               )
+    fig = px.choropleth_mapbox(
+        geomap_data,
+        geojson=geo_json,
+        color="log_count",
+        color_continuous_scale="RdYlBu_r",
+        locations="zone_name",
+        featureidkey="properties.zone",
+        mapbox_style=STYLE_FILE,
+        hover_data=["count"],
+        zoom=zoom,
+        center=center,
+        opacity=0.7,
+    )
     # Custom tool-tip
-    hovertemplate = '<br>Zone: %{location}' \
-                    '<br>Number of trips: %{customdata:.3s}'
-    fig.data[0]['hovertemplate'] = hovertemplate
+    hovertemplate = "<br>Zone: %{location}" "<br>Number of trips: %{customdata:.3s}"
+    fig.data[0]["hovertemplate"] = hovertemplate
 
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
-                      coloraxis_showscale=False, showlegend=False, clickmode='event+select')
+    fig.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        coloraxis_showscale=False,
+        showlegend=False,
+        clickmode="event+select",
+    )
     return fig
+
 
 # RIDES PER DAY
 
@@ -107,8 +116,9 @@ def create_figure_geomap(pickup_counts, zoom=10, center={"lat": 40.7, "lon": -73
 def create_daily_plot(daily):
 
     weekly_fig = px.bar(x=DAYS, y=daily)
-    weekly_fig.update_yaxes(zeroline=True, showgrid=True,
-                            gridwidth=.5, gridcolor='#d5d5d5')
+    weekly_fig.update_yaxes(
+        zeroline=True, showgrid=True, gridwidth=0.5, gridcolor="#d5d5d5"
+    )
 
     weekly_fig.update_layout(CHART_LAYOUT)
 
@@ -118,19 +128,21 @@ def create_daily_plot(daily):
     )
     return weekly_fig
 
+
 # RIDES PER HOUR
 
 
 def create_hourly_plot(hourly):
     hourly_fig = px.area(hourly)
 
-    hourly_fig.update_yaxes(zeroline=True, showgrid=True,
-                            gridwidth=.5, gridcolor='#d5d5d5')
+    hourly_fig.update_yaxes(
+        zeroline=True, showgrid=True, gridwidth=0.5, gridcolor="#d5d5d5"
+    )
 
     hourly_fig.update_layout(CHART_LAYOUT)
 
     hourly_fig.update_layout(
-        yaxis=dict(title=None, tickformat='.1%'),
+        yaxis=dict(title=None, tickformat=".1%"),
         xaxis=dict(title=None),
         showlegend=False,
     )
@@ -139,41 +151,46 @@ def create_hourly_plot(hourly):
 
 
 def create_destination_table(destinations):
-    text_style = {'font-size':13}
+    text_style = {"font-size": 13}
 
-    table_header = [html.Thead(
-        html.Tr([html.Th("Zone", style=text_style), html.Th("%", style=text_style)]))]
+    table_header = [
+        html.Thead(
+            html.Tr([html.Th("Zone", style=text_style), html.Th("%", style=text_style)])
+        )
+    ]
 
     rows = []
 
     for zone, share in destinations.items():
-        if zone != 'Other':
-            row = html.Tr([html.Td(f"{zone}", style=text_style), html.Td(f"{share/10:.1%}", style=text_style)])
+        if zone != "Other":
+            row = html.Tr(
+                [
+                    html.Td(f"{zone}", style=text_style),
+                    html.Td(f"{share/10:.1%}", style=text_style),
+                ]
+            )
             rows.append(row)
-
 
     table_body = [html.Tbody(rows, className="w100")]
 
     return table_header + table_body
+
 
 # AVERAGE FARE
 
 
 def create_heatmap(heatmap):
 
-    heatmap_fig = px.imshow(heatmap, x=list(range(24)),
-                            y=DAYS, color_continuous_scale='RdYlBu')
+    heatmap_fig = px.imshow(
+        heatmap, x=list(range(24)), y=DAYS, color_continuous_scale="RdYlBu"
+    )
 
     heatmap_fig.update_layout(CHART_LAYOUT)
 
-    heatmap_fig.update_layout(
-        coloraxis=dict(reversescale=True, showscale=False)
-    )
+    heatmap_fig.update_layout(coloraxis=dict(reversescale=True, showscale=False))
 
-    hovertemplate = '<br>Hour: %{x}' \
-                    '<br>Day: %{y}' \
-                    '<br>Price: %{z}'
-    heatmap_fig.data[0]['hovertemplate'] = hovertemplate
+    hovertemplate = "<br>Hour: %{x}" "<br>Day: %{y}" "<br>Price: %{z}"
+    heatmap_fig.data[0]["hovertemplate"] = hovertemplate
 
     return heatmap_fig
 
@@ -184,17 +201,17 @@ def create_heatmap(heatmap):
 
 # Select the data for the entire city
 data = ZONE_DATA.get(-1)  # -1 is code for the entire city
-pickup_counts = data.get('pickup_counts')
+pickup_counts = data.get("pickup_counts")
 
 # Create initial plot
 chart = create_figure_geomap(pickup_counts)
-daily = create_daily_plot(data.get('daily'))
-hourly = create_hourly_plot(data.get('hourly'))
-destinations = create_destination_table(data.get('destinations'))
-heatmap = create_heatmap(data.get('heatmap'))
+daily = create_daily_plot(data.get("daily"))
+hourly = create_hourly_plot(data.get("hourly"))
+destinations = create_destination_table(data.get("destinations"))
+heatmap = create_heatmap(data.get("heatmap"))
 
 
-def build_graph_element(figure, title=None, id='', style=dict(), **kwargs):
+def build_graph_element(figure, title=None, id="", style=dict(), **kwargs):
 
     graph = dcc.Graph(
         figure=figure,
@@ -209,10 +226,8 @@ def build_graph_element(figure, title=None, id='', style=dict(), **kwargs):
         content = graph
 
     return dbc.Card(
-        dbc.CardBody(
-            content
-        ),
-        color='light',
+        dbc.CardBody(content),
+        color="light",
         outline=False,
     )
 
@@ -221,24 +236,26 @@ def build_graph_element(figure, title=None, id='', style=dict(), **kwargs):
 #   App layout
 #
 map = dbc.Card(
-    dbc.CardBody(
-        dcc.Graph(
-            id='nyc_map',
-            figure=chart,
-            style={'height': '80vh'}
-        ))
+    dbc.CardBody(dcc.Graph(id="nyc_map", figure=chart, style={"height": "80vh"}))
 )
 
 table = dbc.Card(
     [
-        dbc.CardBody([
-            html.H6("Top destination zones"),
-            dbc.Table(
-                destinations, bordered=False, size='sm', id='destinations', striped=True,
-            )]
+        dbc.CardBody(
+            [
+                html.H6("Top destination zones"),
+                dbc.Table(
+                    destinations,
+                    bordered=False,
+                    size="sm",
+                    id="destinations",
+                    striped=True,
+                ),
+            ]
         )
-    ], 
-    color='light', outline=False
+    ],
+    color="light",
+    outline=False,
 )
 
 
@@ -246,10 +263,16 @@ main_dash = dbc.Card(
     [
         dbc.CardHeader(
             children=[
-                html.H4("New York City", id='zone-stats-header',
-                        style={"float": "left"}),
-                dbc.Button("Reset", id='reset-button', outline=True,
-                           color='secondary', style={"float": "right"})
+                html.H4(
+                    "New York City", id="zone-stats-header", style={"float": "left"}
+                ),
+                dbc.Button(
+                    "Reset",
+                    id="reset-button",
+                    outline=True,
+                    color="secondary",
+                    style={"float": "right"},
+                ),
             ],
         ),
         dbc.CardBody(
@@ -257,23 +280,33 @@ main_dash = dbc.Card(
                 dbc.CardDeck(
                     [
                         build_graph_element(
-                            daily, id='daily', title="Rides per day compared to average", style={
-                                'height': '30vh'}),
-                        build_graph_element(
-                            hourly, id='hourly', title="Ride frequency throughout the day", 
-                            style={'height': '25vh'}
-                            )
-                    ],
-                    className="mb-3"
-                ),
-                dbc.CardDeck([
-                    build_graph_element(
-                        heatmap, id='heatmap', title='Average total fare', 
-                        style={'height': '25vh'}
+                            daily,
+                            id="daily",
+                            title="Rides per day compared to average",
+                            style={"height": "30vh"},
                         ),
-                    table]),
+                        build_graph_element(
+                            hourly,
+                            id="hourly",
+                            title="Ride frequency throughout the day",
+                            style={"height": "25vh"},
+                        ),
+                    ],
+                    className="mb-3",
+                ),
+                dbc.CardDeck(
+                    [
+                        build_graph_element(
+                            heatmap,
+                            id="heatmap",
+                            title="Average total fare",
+                            style={"height": "25vh"},
+                        ),
+                        table,
+                    ]
+                ),
             ]
-        )
+        ),
     ]
 )
 
@@ -281,51 +314,61 @@ BUTTON_STYLE = dict(style={"color": "grey"})
 
 
 # Navigation buttons
-navbar = dbc.Navbar([
-    dbc.NavbarBrand(html.H2("NYC Taxi Dashboard")),
-    dbc.NavbarToggler(id="navbar-toggler"),
-    dbc.Collapse(dbc.Nav(
-        [
-            dbc.NavItem(dbc.NavLink("Description", id='description-button',
-                                    active=True, href="#", **BUTTON_STYLE)),
-            dbc.NavItem(dbc.NavLink("Dataset", href="https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page", **BUTTON_STYLE)),
-            dbc.NavItem(dbc.NavLink(
-                "Repository", href="https://github.com/sebastjancizel/nyc-taxi-dashboard", **BUTTON_STYLE)),
-        ],
-        style={'color': 'grey'}, fill=True, 
-    ),
-    id='navbar-collapse', navbar=True) 
-])
+navbar = dbc.Navbar(
+    [
+        dbc.NavbarBrand(html.H2("NYC Taxi Dashboard")),
+        dbc.NavbarToggler(id="navbar-toggler"),
+        dbc.Collapse(
+            dbc.Nav(
+                [
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            "Description",
+                            id="description-button",
+                            active=True,
+                            href="#",
+                            **BUTTON_STYLE,
+                        )
+                    ),
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            "Dataset",
+                            href="https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page",
+                            **BUTTON_STYLE,
+                        )
+                    ),
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            "Repository",
+                            href="https://github.com/sebastjancizel/nyc-taxi-dashboard",
+                            **BUTTON_STYLE,
+                        )
+                    ),
+                ],
+                style={"color": "grey"},
+                fill=True,
+            ),
+            id="navbar-collapse",
+            navbar=True,
+        ),
+    ]
+)
 description = dbc.Modal(
     [
         dbc.ModalHeader("Description"),
         dbc.ModalBody(dcc.Markdown(DESCRIPTION)),
-        dbc.ModalFooter(
-            dbc.Button("Close", id="close-description")
-        )
+        dbc.ModalFooter(dbc.Button("Close", id="close-description")),
     ],
     id="description",
-    size="xl"
+    size="xl",
 )
 
 content = html.Div(
-    dbc.Card(
-        [
-            dbc.CardBody(
-                dbc.CardDeck(
-                    [
-                        map,
-                        main_dash
-                    ]
-                ))
-        ],
-        id='content'
-    )
+    dbc.Card([dbc.CardBody(dbc.CardDeck([map, main_dash]))], id="content")
 )
 
 
-app.layout = html.Div(children=[dcc.Location(
-    id="url"), navbar, content, description])
+app.layout = html.Div(children=[dcc.Location(id="url"), navbar, content, description])
 
 #
 # CALLBACKS
@@ -333,42 +376,43 @@ app.layout = html.Div(children=[dcc.Location(
 
 # Chart update
 @app.callback(
-    Output('zone-stats-header', 'children'),
-    Output('daily', 'figure'),
-    Output('hourly', 'figure'),
-    Output('destinations', 'children'),
-    Output('heatmap', 'figure'),
-    Input('nyc_map', 'clickData'),
-    Input('reset-button', 'n_clicks')
+    Output("zone-stats-header", "children"),
+    Output("daily", "figure"),
+    Output("hourly", "figure"),
+    Output("destinations", "children"),
+    Output("heatmap", "figure"),
+    Input("nyc_map", "clickData"),
+    Input("reset-button", "n_clicks"),
 )
 def update_content(clickData, n_clicks):
 
     changed_id = dash.callback_context.triggered[0]
 
-    if 'reset-button.n_clicks' == changed_id.get('prop_id', ''):
+    if "reset-button.n_clicks" == changed_id.get("prop_id", ""):
         zone = -1  # Get aggregate data
     else:
-        zone = clickData['points'][0]['pointNumber'] if clickData is not None else -1
+        zone = clickData["points"][0]["pointNumber"] if clickData is not None else -1
     data = ZONE_DATA.get(zone)
-    daily = create_daily_plot(data.get('daily'))
-    hourly = create_hourly_plot(data.get('hourly'))
-    destinations = create_destination_table(data.get('destinations'))
-    heatmap = create_heatmap(data.get('heatmap'))
+    daily = create_daily_plot(data.get("daily"))
+    hourly = create_hourly_plot(data.get("hourly"))
+    destinations = create_destination_table(data.get("destinations"))
+    heatmap = create_heatmap(data.get("heatmap"))
 
-    zone_name = data['name'] if clickData is not None else "New York City"
+    zone_name = data["name"] if clickData is not None else "New York City"
     return zone_name, daily, hourly, destinations, heatmap
 
-# Toggle description 
+
+# Toggle description
 @app.callback(
-    Output('description', 'is_open'),
-    [Input('description-button', 'n_clicks'),
-     Input('close-description', 'n_clicks')],
-    [State('description', 'is_open')]
+    Output("description", "is_open"),
+    [Input("description-button", "n_clicks"), Input("close-description", "n_clicks")],
+    [State("description", "is_open")],
 )
 def toggle_description(n1, n2, is_open):
     if n1 or n2:
         return not is_open
     return is_open
+
 
 # Control the collapse of navbar on small screens
 @app.callback(
@@ -382,5 +426,5 @@ def toggle_navbar_collapse(n, is_open):
     return is_open
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True, port=8080)
--- getdata.py
+++ getdata.py
@@ -3,27 +3,32 @@ import json
 import numpy as np
 from tqdm import tqdm
 
-zone_filename = 'aux_data/zone.json'
+zone_filename = "aux_data/zone.json"
 with open(zone_filename) as f:
     zmapper = json.load(f)
 
 zone_index_to_name = {int(index): name for index, name in zmapper.items()}
 
 # Open the main data
-TAXI_PATH = 's3://vaex/taxi/yellow_taxi_2012_zones.hdf5?anon=true'
+TAXI_PATH = "s3://vaex/taxi/yellow_taxi_2012_zones.hdf5?anon=true"
 df = vaex.open(TAXI_PATH)
 
-used_columns = ['total_amount',
-                'trip_duration_min',
-                'trip_speed_mph',
-                'trip_distance',
-                'pickup_hour',
-                'pickup_day',
-                'dropoff_zone',
-                'pickup_zone']
+used_columns = [
+    "total_amount",
+    "trip_duration_min",
+    "trip_speed_mph",
+    "trip_distance",
+    "pickup_hour",
+    "pickup_day",
+    "dropoff_zone",
+    "pickup_zone",
+]
 
-df.categorize(df.pickup_day, labels=[
-    'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], inplace=True)
+df.categorize(
+    df.pickup_day,
+    labels=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    inplace=True,
+)
 df.categorize(df.pickup_zone, inplace=True)
 df.categorize(df.dropoff_zone, inplace=True)
 df.categorize(df.pickup_hour, inplace=True)
@@ -39,35 +44,39 @@ def extract_ride_statistics(zone=None):
 
     # Compute relative share of rides for each day compared to the mean
     daily_data = df.count(binby=df.pickup_day, selection=selection)
-    daily_data = 1/sum(daily_data) * daily_data
+    daily_data = 1 / sum(daily_data) * daily_data
     daily_data = np.around(daily_data - np.mean(daily_data), decimals=3)
 
     # Compute relative share of pickups each hour
     hourly_data = df.count(binby=df.pickup_hour, selection=selection)
-    hourly_data = np.around(1/sum(hourly_data) * hourly_data, decimals=3)
+    hourly_data = np.around(1 / sum(hourly_data) * hourly_data, decimals=3)
 
     # Compute top 5 destination zones
     dropoff_count = df.count(binby=df.dropoff_zone, selection=selection)
-    dropoff_count = 1/sum(dropoff_count) * dropoff_count
+    dropoff_count = 1 / sum(dropoff_count) * dropoff_count
     # - is because we want the maximum elements
     sorted_args = np.argsort(-dropoff_count)
     max_args = sorted_args[:5]
     top_destinations = {
-        zone_index_to_name[zone]: np.round(dropoff_count[zone], 3) for zone in max_args}
-    top_destinations['Other'] = 10 * \
-        np.round(np.sum([dropoff_count[zone] for zone in sorted_args[5:]]), 3)
+        zone_index_to_name[zone]: np.round(dropoff_count[zone], 3) for zone in max_args
+    }
+    top_destinations["Other"] = 10 * np.round(
+        np.sum([dropoff_count[zone] for zone in sorted_args[5:]]), 3
+    )
 
     name = zone_index_to_name[zone] if zone is not None else "New York City"
 
-    heatmap_data = df.total_amount.mean(binby=['pickup_day', 'pickup_hour'], selection=selection)
-    heatmap_data = np.round(heatmap_data, 3) # this puts Monday on top
+    heatmap_data = df.total_amount.mean(
+        binby=["pickup_day", "pickup_hour"], selection=selection
+    )
+    heatmap_data = np.round(heatmap_data, 3)  # this puts Monday on top
 
     data = {
         "name": name,
         "daily": daily_data.tolist(),
         "hourly": hourly_data.tolist(),
         "destinations": top_destinations,
-        "heatmap": heatmap_data.tolist()
+        "heatmap": heatmap_data.tolist(),
     }
 
     return data
@@ -80,10 +89,10 @@ for zone in tqdm(zone_index_to_name):
 
 # Store total data
 ZONE_DATA[-1] = extract_ride_statistics()
-ZONE_DATA[-1]['pickup_counts'] = df.count(binby=df.pickup_zone).tolist()
+ZONE_DATA[-1]["pickup_counts"] = df.count(binby=df.pickup_zone).tolist()
 
 
 OUT_PATH = "./aux_data/zone_data.json"
 
-with open(OUT_PATH, 'w') as f:
+with open(OUT_PATH, "w") as f:
     json.dump(ZONE_DATA, f)
